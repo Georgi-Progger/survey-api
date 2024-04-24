6c545ce431958038b81cc946dd3469b0179d0a13
tanduev <tanduevdmitriy@gmail.com>
2024-04-24 08:37:34 +0300

    resume upload

--- internal/handler/candidate.go
+++ internal/handler/candidate.go
@@ -7,13 +7,54 @@ import (
 	model "github.com/Georgi-Progger/survey-api/internal/model"
 	"github.com/Georgi-Progger/survey-api/pkg/jwt"
 	"github.com/labstack/echo/v4"
+
+	. "github.com/Georgi-Progger/survey-api/pkg/s3storage"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/google/uuid"
 )
 
 func (h *Handler) InsertCandidate(c echo.Context) error {
+	sess, err := CreateSession()
+	if err != nil {
+		log.Println("Failed to create session:", err)
+		return c.JSON(http.StatusInternalServerError, map[string]string{"err": "Failed to open file"})
+	}
+
+	svc := s3.New(sess)
+
+	file, err := c.FormFile("file")
+	if err != nil {
+		log.Println("Failed to open file:", err)
+		return c.JSON(http.StatusInternalServerError, map[string]string{"err": "Failed to open file"})
+	}
+	src, err := file.Open()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"err": "Failed to read file"})
+	}
+	defer src.Close()
+	fileName := uuid.New().String()
+	size := file.Size
+
+	buffer := make([]byte, size)
+	_, err = src.Read(buffer)
+	if err != nil {
+		log.Println("Failed to read file:", err)
+		return c.JSON(http.StatusInternalServerError, map[string]string{"err": "Failed to read file"})
+	}
+
+	contentType := http.DetectContentType(buffer)
+
+	err = UploadToS3(svc, buffer, fileName, contentType)
+	if err != nil {
+		log.Println("Failed to upload file to S3:", err)
+		return c.JSON(http.StatusInternalServerError, map[string]string{"err": "Failed to upload file to S3"})
+	}
+
 	var candidate model.Candidate
 	if err := c.Bind(&candidate); err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid JSON"})
 	}
+	candidate.ResumePath = fileName
 	candidate.UserId = jwt.GetUserIdFromContext(c)
 	if err := h.services.Candidate.Create(c.Request().Context(), candidate); err != nil {
 		log.Println(err)
